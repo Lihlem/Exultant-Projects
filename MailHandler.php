0c0e06c711c4797fca5316a7d905cd7a7a41779e
Lihlem <istore2104@gmail.com>
2016-06-13 16:50:14 +0200

    Update MailHandler.php

--- MailHandler.php
+++ MailHandler.php
@@ -2,7 +2,7 @@
 /*/originally owner_email/*/
 //$owner_email = $_POST["owner_email"]; 
 
-$owner_email = 'mazazacreativesolutions@gmail.com';
+$owner_email = 'mazazacreativesolutions@gmail.com'; //your email
 $headers = 'From:' . @$_POST['email'];
 $subject = 'Enquiry from the website Mazaza Creative Solutions' . @$_POST["name"];
 $messageBody = "";
@@ -31,7 +31,7 @@ try{
     if(!mail($owner_email, $subject, $messageBody, $headers)){
         throw new Exception('An unexpected error occurred with the email being sent!<br />Please try again contact us directly at <a href="mailto:kmakis@mazazacreativesolutions.co.za');
     }else{
-        header("location:contact-thanks.php");
+        header("location:contact-thanks.php");// create a separate page.
     }
 }catch(Exception $e){
     echo $e->getMessage() ."\n";
